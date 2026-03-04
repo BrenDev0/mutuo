f1a8ea6d3bdc00fa0c144269b9e11fbdca7b5093
Brendan Lahey <lahey1991@gmail.com>
2026-03-04 09:24:15 -0600

    complete

--- src/features/users/infrastructure/sqlalchemy/users_repository.py
+++ src/features/users/infrastructure/sqlalchemy/users_repository.py
@@ -1,20 +1,22 @@
-from uuid import uuid4
-from sqlalchemy import Column, String, DateTime, func
+import uuid
+from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
 from src.persistance import SqlAlchemyBase, AsyncSqlAlchemyDataRepository
 from ...domain import User
 
 class SqlAlchemyUser(SqlAlchemyBase):
     __tablename__ = "Users"
 
-    user_id=Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    name=Column(String, nullable=False)
-    email=Column(String, nullable=False)
-    email_hash=Column(String, nullable=False)
-    phone=Column(String, nullable=False)
-    profile_type=Column(String, nullable=False)
-    password=Column(String, nullable=False)
-    created_at=Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    email_hash: Mapped[str] = mapped_column(String, nullable=False)
+    phone: Mapped[str] = mapped_column(String, nullable=False)
+    profile_type: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
     
 class SqlAlchemyUserRepository(AsyncSqlAlchemyDataRepository[User, SqlAlchemyUser]):
     def __init__(self):
